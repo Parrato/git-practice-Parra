e00500a62e7ec3e4b84272f5f0a6fd1c3fc49d55
Arturo Parra <arturojoseparra8@gmail.com>
2026-01-29 20:07:42 -0400

    feat: crear programa hola mundo inicial

---
+++ Programacion C++ Git/.vscode/hola_mundo.cpp
@@ -0,0 +1,20 @@
+// hola mundo.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    std::string nombre;
+
+    std::cout << "¡Hola Mundo desde Git! Cual es mi nombre?" << endl;
+
+    std::cin >> nombre;
+
+
+    std::cout << "Mi nombre es: " << nombre << std::endl;
+
+
+
+    return 0;
+}
